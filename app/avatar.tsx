8940b74312369d818aeb1ccb442d546d14043967
Daniel Carlyle <dan.carlyle@gmail.com>
2024-12-05 13:13:58 -0500

    first commit

--- app/avatar.tsx
+++ app/avatar.tsx
@@ -1,3 +1,4 @@
+/*
 import ContentfulImage from "@/lib/contentful-image";
 
 export default function Avatar({
@@ -22,3 +23,32 @@ export default function Avatar({
     </div>
   );
 }
+  */
+
+import ContentfulImage from "@/lib/contentful-image";
+
+export default function Avatar({
+  name,
+  picture,
+}: {
+  name: string;
+  picture: { url?: string } | null;
+}) {
+  const imageUrl = picture?.url || "/fallback-image.png"; // Fallback to default image if URL is not present.
+
+  return (
+    <div className="flex items-center">
+      <div className="mr-4 w-12 h-12">
+        <ContentfulImage
+          alt={name}
+          className="object-cover h-full rounded-full"
+          height={48}
+          width={48}
+          src={imageUrl}
+        />
+      </div>
+      <div className="text-xl font-bold">{name}</div>
+    </div>
+  );
+}
+
